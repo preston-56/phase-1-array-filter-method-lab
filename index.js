d9694ce1b369e59ee873f6f5397095c3ed1b91ab
Preston <prestonosoro56@gmail.com>
2022-11-30 01:09:18 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers,  name){
+   const filteredDrivers =  drivers.filter((driverName) => driverName.toLowerCase() === name.toLowerCase())
+    return filteredDrivers
+}
+
+
+ function fuzzyMatch(drivers, substring) {
+    const matchedDrivers = drivers.filter(driver => driver.slice(0, substring.length)  === substring)
+    return matchedDrivers
+ }
+
+ function matchName(driver,hometown){
+    const results = driver.filter(element => element.name === hometown )
+    return results
+}
